4436130dafa711c64c942ab3d90dfb0454f663c3
bonveeee <bonochieng@gmail.com>
2023-01-12 21:02:05 +0300

    add toggling to buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from "./components/AddTask";
 
 function App() {
 
+  const  [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -38,13 +39,16 @@ function App() {
 
   //add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
   }
 
   return (
     <div className="container">
-    <Header title = "Task Tracker"/>
-    <AddTask onAdd={addTask}/>
+    <Header title = "Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+
+    {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0? (
       <Tasks tasks={tasks} onDelete= {deleteTask} onToggle ={toggleReminder} />
       ) : 
